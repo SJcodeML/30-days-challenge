55207df433174d59f6a6b1274b246c70e2a44f84
SJcodeML <sidrajabin56@gmail.com>
2024-10-01 13:37:01 +0500

    added project 5 and 6

--- 17-meme-generator-app/src/components/meme-generator.tsx
+++
@@ -1,6 +0,0 @@
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Textarea} from "@/components/ui/textarea";
-import { Card , CardContent , CardHeader , CardTitle } from "@/components/ui/card";
-
-  
